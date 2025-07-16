39e25f19934dca56875097e03e31bb00b701e7d7
J2Doeyok <j2doeyok@gmail.com>
2025-07-16 12:56:28 +0100

    calculator version 1

---
+++ cal_app.py
@@ -0,0 +1,71 @@
+# A simple calculator application.
+
+print('''***********************************************
+Welcome to my simple calculator app. 🤗\n1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+*******************************************************''')
+# Addition operation
+
+print("Enter two numbers to perform addition operation")
+# Prompt user to enter first & second number and store it
+first_number = input("first_number:\n>>")
+second_number = input("second_number:\n>>")
+sum = float(first_number) + float(second_number)
+print(sum)
+print(f"{first_number} + {second_number} = {sum}")
+
+# Subtraction operation
+
+print("***************************************")
+print("Subtraction")
+print("Enter two numbers to perform subtraction operation")
+# Prompt user to enter first & second number and store it
+first_number = input("first_number:\n>>")
+second_number = input("second_number:\n>>")
+sub = float(first_number) - float(second_number)
+print(sub)
+print(f"{first_number} + {second_number} = {sub}")
+
+# Multiplication operation
+ 20 
+ 21 print("***************************************")
+ 22 print("Multiplication")
+ 23 print("Enter two numbers to perform multiplication operation")
+ 24 # Prompt user to enter first & second number and store it
+ 25 first_number = input("first_number:\n>>")
+ 26 second_number = input("second_number:\n>>")
+ 27 sub = float(first_number) * float(second_number)
+ 28 print(mul)
+ 29 print(f"{first_number} * {second_number} = {mul}")
+
+# Division operation
+ 20
+ 21 print("***************************************")
+ 22 print("Division")
+ 23 print("Enter two numbers to perform division operation")
+ 24 # Prompt user to enter first & second number and store it
+ 25 first_number = input("first_number:\n>>")
+ 26 second_number = input("second_number:\n>>")
+ 27 sub = float(first_number) / float(second_number)
+ 28 print(div)
+ 29 print(f"{first_number} / {second_number} = {div}")
+
+# Exponentiation operation
+ 20
+ 21 print("***************************************")
+ 22 print("Exponentiation")
+ 23 print("Enter two numbers to perform exponentiation operation")
+ 24 # Prompt user to enter first & second number and store it
+ 25 first_number = input("first_number:\n>>")
+ 26 second_number = input("second_number:\n>>")
+ 27 sub = float(first_number) ** float(second_number)
+ 28 print(exp)
+ 29 print(f"{first_number} ** {second_number} = {exp}")
+
+
+
+
+
+
