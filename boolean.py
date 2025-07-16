d38c0d3f8bf527d4581c9cf0488119783dc05e03
J2Doeyok <j2doeyok@gmail.com>
2025-07-16 11:33:37 +0100

    bool on int

--- boolean.py
+++ boolean.py
@@ -1,8 +1,7 @@
-
-
+# boolean
 isMarried = True
 isMarried = False
-
+'''
 name = "Grace"
 first_name = "Jerry"
 print(bool(name))
@@ -12,3 +11,15 @@ goods = ""
 team = ""
 print(bool(goods))
 print(bool(team))
+'''
+# integer
+
+number = 0.5 #True
+print(bool(number))
+number_1 = 45 #True
+print(bool(number_1))
+
+number_2 = 0 #False
+print(bool(number_2))
+number_3 = 0.0 #False
+print(bool(number_3))
