7727ae6bc6cf8bcf1a7d0cb6b22cbf45baa43878
J2Doeyok <j2doeyok@gmail.com>
2025-07-16 11:13:17 +0100

    my python class activity

---
+++ boolean.py
@@ -0,0 +1,14 @@
+
+
+isMarried = True
+isMarried = False
+
+name = "Grace"
+first_name = "Jerry"
+print(bool(name))
+print(bool(first_name))
+
+goods = ""
+team = ""
+print(bool(goods))
+print(bool(team))
